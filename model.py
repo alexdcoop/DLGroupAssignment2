bb33959f729dc6e66a0dee1ec490d215e00bb546
Alexander Cooper <acoope42@vols.utk.edu>
2022-04-06 14:27:29 -0400

    Model updates

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 #This is the final model file
+from ast import Expression
 import tensorflow as tf
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GridSearchCV
@@ -38,5 +42,62 @@ def createNeuralNet(batchSize, learning_rate, numHiddenLayers, numHiddenNeurons,
     return model
     
 #Make Grid of all possible parameters
-param_grid = [{'batch_size':[100000, 50000, 10000, 5000, 1000], 'learning_rate':[.001,.01,.1,.2,.3], 'num_hid_layers':[5,4,3,2,1], 'num_hid_neurons':[200, 150, 125, 100, 75, 50, 25, 10, 5], 'hid_activation':['sigmoid', 'tanh','relu','elu','leaky relu','elu'], 'optimizer':['plain SGD','momentum','nesterov','adagrad','rmsprop','adam','learning rate scheduling']}]
+param_grid = [{'learning_rate':[.001,.01,.1,.2,.3], 'num_hid_layers':[5,4,3,2,1], 'num_hid_neurons':[200, 150, 125, 100, 75, 50, 25, 10, 5], 'hid_activation':['sigmoid', 'tanh','relu','elu','leaky relu','elu'], 'optimizer':['plain SGD','momentum','nesterov','adagrad','rmsprop','adam','learning rate scheduling']}]
 
+
+def createModel(learning_rate, num_hid_layers, num_hid_neurons, hid_activation,optimizer):
+    #Create model
+    model = keras.Sequential()
+    
+    #Add input layer
+    model.add(keras.Input(shape=(X.shape[1],)))
+    
+    #Add dense layers
+    for layer in range(num_hid_layers):
+        model.add(layers.Dense(units=num_hid_neurons, activation=hid_activation))
+        
+    #output layer
+    model.add(layers.Dense(units=1, activation = "linear"))
+    
+    #make opimizer
+    if optimizer == 'plain SGD':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate)
+    elif optimizer == 'momentum':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9) #Hardcoded optimizer
+    elif optimizer == 'nesterov':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9, nesterov=True)
+    elif optimizer == 'adagrad':
+        optimizerCode = tf.keras.optimzers.Adagrad(learning_rate=learning_rate, initial_accumulator_value=0.1, epsilon=1e-07)
+    elif optimizer == 'rmsprop':
+        optimizerCode = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=.9,momentum=0.0,epsilon=1e-07)
+    elif optimizer == 'adam':
+        optimizerCode = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=.9,beta_2=.999,epsilon=1e-07)
+    elif optimizer == 'learning rate scheduling':
+        initial_learning_rate = learning_rate; decay_steps = 10000; decay_rate = .95
+        learning_sched = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate,decay_steps, decay_rate)
+        optimizerCode = tf.keras.optimzers.SGD(learning_rate=learning_sched)
+    else:
+        print('Error making optimizer')
+        return -1
+    
+    #compile model
+    model.compile(loss = 'mse', optimizer=optimizerCode, metrics=['accuracy'])
+    
+    return model
+
+#Batch sizes
+{'batch_size':[100000, 50000, 10000, 5000, 1000]}
+
+model = KerasClassifier(build_fn=createModel, epochs=100, batch_size=1000, verbose=0)
+
+#Grid search
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+gridResult = grid.fit(X,Y)
+
+# summarize results
+print("Best: %f using %s" % (gridResult.best_score_, gridResult.best_params_))
+means = gridResult.cv_results_['mean_test_score']
+stds = gridResult.cv_results_['std_test_score']
+params = gridResult.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
