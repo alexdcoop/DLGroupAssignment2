72530154131126fdfcee6208a0f56a50702fadb0
Alexander Cooper <acoope42@vols.utk.edu>
2022-04-12 09:41:10 -0400

    More changes

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 #This is the final model file
+from tabnanny import verbose
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -22,45 +23,61 @@ X = np.array(np.column_stack((DATA['sku'],DATA['price'],DATA['order'],DATA['dura
 # Split into train and test
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
 
+#Scale the data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-# def createNeuralNet(learning_rate, num_hid_layers, num_hid_neurons, hid_activation, optimizer):
-#     inputs = tf.keras.layers.Input(shape=(X.shape[1],), name='input') #Note: shape is a tuple and does not include records. For a two dimensional input dataset, use (Nbrvariables,). We would use the position after the comma, if it would be a 3-dimensional tensor (e.g., images). Note that (something,) does not create a second dimension. It is just Python's way of generating a tuple (which is required by the Input layer).
-#     hidden = tf.keras.layers.Dense(units=num_hid_neurons, activation=hid_activation)(inputs)
+
+def createNeuralNet(learning_rate, num_hid_layers, num_hid_neurons, hid_activation, optimizer):
+    shape = 1
+    #Create inputs layer
+    inputs_cat = tf.keras.layers.Input(shape=(shape,),name = 'in_cat')
+    inputs_quant = tf.keras.layers.Input(shape=(shape,),name = 'in_quant')
+    embedding_category = tf.keras.layers.Embedding(input_dim=32, output_dim=26, input_length=1,name = 'embedding_category')(inputs_cat)
+    embedding_sku = tf.keras.layers.Embedding(input_dim=74999, output_dim=50000, input_length=1,name = 'embedding_sku')(inputs_quant)
+    embedding = tf.keras.layers.Concatenate(name = 'concatenation_category')([embedding_category, embedding_sku])
+    embedding_flat = tf.keras.layers.Flatten(name='flatten')(embedding)
+    inputs_num = tf.keras.layers.Input(shape=(shape,),name = 'in_num')
+    inputs = tf.keras.layers.Concatenate(name = 'concatenation')([embedding_flat, inputs_num])
     
-#     for layer in range(num_hid_layers):
-#         hidden = tf.keras.layers.Dense(units=num_hid_neurons, activation=hid_activation)(hidden)
-
-#     output = tf.keras.layers.Dense(units=1, activation = "linear", name= 'output')(hidden)
+    hidden = tf.keras.layers.Dense(units=num_hid_neurons, activation=hid_activation)(inputs)
     
-#     #Create model 
-#     model = tf.keras.Model(inputs = inputs, outputs = output)
+    for layer in range(num_hid_layers - 1):
+        hidden = tf.keras.layers.Dense(units=num_hid_neurons, activation=hid_activation)(hidden)
+
+    output = tf.keras.layers.Dense(units=1, activation = "linear", name= 'output')(hidden)
     
-#     #make opimizer
-#     if optimizer == 'plain SGD':
-#         optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate)
-#     elif optimizer == 'momentum':
-#         optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9) #Hardcoded optimizer
-#     elif optimizer == 'nesterov':
-#         optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9, nesterov=True)
-#     elif optimizer == 'adagrad':
-#         optimizerCode = tf.keras.optimizers.Adagrad(learning_rate=learning_rate, initial_accumulator_value=0.1, epsilon=1e-07)
-#     elif optimizer == 'rmsprop':
-#         optimizerCode = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=.9,momentum=0.0,epsilon=1e-07)
-#     elif optimizer == 'adam':
-#         optimizerCode = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=.9,beta_2=.999,epsilon=1e-07)
-#     elif optimizer == 'learning rate scheduling':
-#         initial_learning_rate = learning_rate; decay_steps = 10000; decay_rate = .95
-#         learning_sched = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate,decay_steps, decay_rate)
-#         optimizerCode = tf.keras.optimizers.SGD(learning_rate=learning_sched)
-#     else:
-#         print('Error making optimizer')
-#         return -1
+    #Create model 
+    model = tf.keras.Model(inputs = inputs, outputs = output)
+    
+    #make opimizer
+    if optimizer == 'plain SGD':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate)
+    elif optimizer == 'momentum':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9) #Hardcoded optimizer
+    elif optimizer == 'nesterov':
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate = learning_rate, momentum = .9, nesterov=True)
+    elif optimizer == 'adagrad':
+        optimizerCode = tf.keras.optimizers.Adagrad(learning_rate=learning_rate, initial_accumulator_value=0.1, epsilon=1e-07)
+    elif optimizer == 'rmsprop':
+        optimizerCode = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=.9,momentum=0.0,epsilon=1e-07)
+    elif optimizer == 'adam':
+        optimizerCode = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=.9,beta_2=.999,epsilon=1e-07)
+    elif optimizer == 'learning rate scheduling':
+        initial_learning_rate = learning_rate; decay_steps = 10000; decay_rate = .95
+        learning_sched = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate,decay_steps, decay_rate)
+        optimizerCode = tf.keras.optimizers.SGD(learning_rate=learning_sched)
+    else:
+        print('Error making optimizer')
+        return -1
 
-#     #Compile model
-#     model.compile(loss = 'mse', optimizer = optimizerCode)
+    #Compile model
+    model.compile(loss = 'mae', optimizer = optimizerCode)
 
-#     #return model
-#     return model
+    #return model
+    return model
     
 #Make Grid of all possible parameters
 #param_grid = [{'learning_rate':[.001,.01,.1,.2,.3], 'num_hid_layers':[5,4,3,2,1], 'num_hid_neurons':[75, 50, 25, 10, 5], 'hid_activation':['sigmoid', 'tanh','relu','elu'], 'optimizer':['plain SGD','momentum','nesterov','adagrad','rmsprop','adam','learning rate scheduling']}]
@@ -90,7 +107,7 @@ def createModel(learning_rate, num_hid_layers, num_hid_neurons, hid_activation, 
         model.add(layers.Dense(units=num_hid_neurons, activation=hid_activation, kernel_initializer=initializer))
         
     #output layer
-    model.add(layers.Dense(units=1, activation = "softmax",kernel_initializer=initializer))
+    model.add(layers.Dense(units=1, activation = "linear",kernel_initializer=initializer))
     
     #make opimizer
     if optimizer == 'plain SGD':
@@ -119,35 +136,38 @@ def createModel(learning_rate, num_hid_layers, num_hid_neurons, hid_activation, 
     return model
 
 
-epochs = 10
+
+combinations = []
+losses = []
+epochs = 100
 batchSize = 100000
 
+#use random subset
+
+
 for record in allCombinations: 
+    #Create Model
     model = createModel(learning_rate = record[0], num_hid_layers = record[1], num_hid_neurons = record[2], hid_activation = record[3], optimizer = record[4], initializer = record[5])
     
-    avgLoss_array = []
-    #Go through each epoch
+    lossList = []
     for _ in range(epochs):
+        #fit model
+        model.fit(X_train,Y_train,epochs=1,batch_size=batchSize, verbose=0)
         
-        lossArr = []
-        #Go through each record of data
-        for _ in range(len(X_train)): #this is the subsetted data that gets pulled into the lower part of the for loop to train the model 
-            #Fit model
-            model.fit(X_train,Y_train,epochs=1,batch_size=batchSize)
-            #Get loss
-            loss = "TBD"
-            #append to lossArr
-            lossArr.append(loss)
+        yhat = model.predict(X_test)
         
-        #Get average loss
-        avg_loss = "TBD"
-    
-        #make a prediction on entire validation set and compute average log_loss
-        avgLoss_array.append(avg_loss) #append average loss to an array additional column in grid 
+        #compute loss (Keep 100 loss values)
+        #manually compute loss
+        #use built in tf mae loss function
         
-        
-    #store loss array in last column of grid grid['new column'] = loss_array (keep a counter) 
+        #append to list
 
+
+
+
+
+    
+        
 
 # #Batch sizes
 # {'batch_size':[100000, 50000, 10000, 5000, 1000]}
@@ -167,8 +187,8 @@ for record in allCombinations:
 #     print("%f (%f) with: %r" % (mean, stdev, param))
     
 # #Random search
-# rnd_search = RandomizedSearchCV(model, param_grid, n_iter =20, cv=3)
-# rnd_search.fit(X,Y)
+#rnd_search = RandomizedSearchCV(model, param_grid, n_iter =20, cv=3)
+#rnd_search.fit(X_train,Y_train)
 
 # # summarize results
 # print("Best: %f using %s" % (rnd_search.best_score_, rnd_search.best_params_))
